396d9961640a6c487fcc8978faf3f9cb8efff649
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-29 11:30:41 +0000

    Here is where I added my event listener function Set JSON object to data I set all the information that I needed to variables to call later Started to insert variables into the elements that i created with id's

--- sand.js
+++ sand.js
@@ -35,50 +35,50 @@ more dynamic than just hardcoding seperate elements in.
 */
 
 
-// let main = document.getElementById('main');
+let main = document.getElementById('main');
 
-// function createDiv(id, parent) {
-//     let div = document.createElement('div');
-//     div.setAttribute('id', id);
-//     parent.appendChild(div);
-// }
-// function createP(id, text, parent) {
-//     let p = document.createElement('p');
-//     p.setAttribute('id', id)
-//     p.textContent = text;
-//     parent.appendChild(p);
-// }
-
-
-// createDiv('static', main);
-// createDiv('titleAndZip', static);
-// createP('weatherTitle', 'Weather App', titleAndZip);
-// createDiv('zipBox', titleAndZip)
-
-// let enterZip = document.createElement('input');
-// enterZip.setAttribute('type', 'number');
-// titleAndZip.appendChild(enterZip);
-// let zipSubmit = document.createElement('button');
-// zipSubmit.textContent = 'Get Weather'
-// zipSubmit.setAttribute('id', 'zipBtn');
-// static.appendChild(zipSubmit);
+function createDiv(id, parent) {
+    let div = document.createElement('div');
+    div.setAttribute('id', id);
+    parent.appendChild(div);
+}
+function createP(id, text, parent) {
+    let p = document.createElement('p');
+    p.setAttribute('id', id)
+    p.textContent = text;
+    parent.appendChild(p);
+}
 
 
-// createDiv('dynamic', main);
-// createDiv('city', dynamic)
-// createP('cityStatic', 'City', city);
-// createP('cityDynamic', 'second P', city);
-// createDiv('temperature', dynamic);
-// createP('tempStatic', 'Temperature', temperature);
-// createP('kTemp', 'Kelvin', temperature);
-// createP('fTemp', 'Farenheit', temperature);
-// createP('cTemp', 'Centigrade', temperature);
-// createDiv('condition', dynamic);
-// createP('staticCondition', 'Condition', condition);
-// createP('dynamicCondition', 'second P', condition);
-// createDiv('icon', dynamic)
-// createP('otherStatic', 'Other Information', icon);
-// createP('otherDynamic', 'second P', icon);
+createDiv('static', main);
+createDiv('titleAndZip', static);
+createP('weatherTitle', 'Weather App', titleAndZip);
+createDiv('zipBox', titleAndZip)
+
+let enterZip = document.createElement('input');
+enterZip.setAttribute('type', 'number');
+titleAndZip.appendChild(enterZip);
+let zipSubmit = document.createElement('button');
+zipSubmit.textContent = 'Get Weather'
+zipSubmit.setAttribute('id', 'zipBtn');
+static.appendChild(zipSubmit);
+
+
+createDiv('dynamic', main);
+createDiv('city', dynamic)
+createP('cityStatic', 'City', city);
+createP('cityDynamic', 'second P', city);
+createDiv('temperature', dynamic);
+createP('tempStatic', 'Temperature', temperature);
+createP('kTemp', 'Kelvin', temperature);
+createP('fTemp', 'Farenheit', temperature);
+createP('cTemp', 'Centigrade', temperature);
+createDiv('condition', dynamic);
+createP('staticCondition', 'Condition', condition);
+createP('dynamicCondition', 'second P', condition);
+createDiv('icon', dynamic)
+createP('otherStatic', 'Other Information', icon);
+createP('otherDynamic', 'second P', icon);
 // ===================================
 // above this is the usable init function guts
 // ====================================
@@ -118,12 +118,11 @@ more dynamic than just hardcoding seperate elements in.
 //this is me turning getWeather into an async function
 //USE THE ASYNC FUNCTION
 //=============================
-// async function getWeather () {
-//     let response = await axios.get ('https://api.openweathermap.org/data/2.5/weather?zip=`${zip}`,us&appid=3fd35bfbfb5f10cf0f3d2a3ec1f270a4')
-//     console.log('before return', response);
-//     return response;
+async function getWeather () {
+    let response = await axios.get ('https://api.openweathermap.org/data/2.5/weather?zip='+'41472'+/*`${zip}`*/',us&appid=3fd35bfbfb5f10cf0f3d2a3ec1f270a4')
+    return response;
 
-// }
+}
 
 /*==========================================================================
 This is the end of getWeather funciton, axios is working and I can point to spacific data within the json object.
@@ -160,11 +159,26 @@ Begining my submitZipButton function
 */
 
 
-    // zipSubmit.addEventListener('click', async () => {
-        //validateZip with the information submited with click
-        //if zip is not valid alert not a valid zip
-        //if zip is valid run getWeather
-        // let data = await getWeather ();
-
-        //use information from getWeather to populate the dynamic elements
-    // })
+    zipSubmit.addEventListener('click', async () => {
+    //     validateZip with the information submited with click
+    //     if zip is not valid alert not a valid zip
+    //     if zip is valid run getWeather
+    //     use information from getWeather to populate the dynamic elements
+        let data = await getWeather ();
+        console.log('after event', data);
+        let cityName = data.data.name;
+        console.log(cityName);
+        let tempK = data.data.main.temp;
+        console.log(tempK);
+        let condition = data.data.weather[0].main;
+        console.log(condition);
+        let icon = data.data.weather[0].icon;
+        console.log(icon);
+        let tempF = ((tempK - 273.15)*(9/5)+(32));
+        console.log(tempF);
+        let tempC = (tempK - 273.15);
+        console.log(tempC);
+        document.getElementById('cityDynamic').textContent = cityName;
+        
+        
+    })
